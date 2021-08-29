d22b9a8279f5efd1433a42f37755a03b19baee6f
Diogo Santana Corazolla <diogocorazolla@gmail.com>
2021-08-29 16:13:07 -0300

    pasta shared - pipes

--- src/app/courses/course.module.ts
+++ src/app/courses/course.module.ts
@@ -3,22 +3,22 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
-import { ReplacePipe } from "../pipe/replace.pipe";
 import { StarModule } from "../shared/component/star/star.module";
+import { AppPipeModule } from "../shared/pipe/app-pipe.module";
 import { CourseInfoComponent } from "./course-info.component";
 import { CourseListComponent } from "./course-list.component";
 
 @NgModule({
     declarations: [
         CourseListComponent,
-        CourseInfoComponent,
-        ReplacePipe
+        CourseInfoComponent
     ],
     imports: [
         BrowserModule,
         CommonModule,
         FormsModule,
         StarModule,
+        AppPipeModule,
         RouterModule.forChild([
             {
                 path: "courses", component: CourseListComponent
